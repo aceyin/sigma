06cdcde43f36e52b9a361cfb1b1663323424809f
aceyin <aceyin@gmail.com>
2020-03-09 00:23:10 +0800

    add config and start shell

--- sigma.sh
+++ sigma.sh
@@ -1,37 +1,90 @@
 #!/usr/bin/env bash
 
-# the full path of this server
-SERVER_BASE=$(dirname "$(readlink -f "$0")")
+## the full path of this server
+#SERVER_BASE=$(dirname "$(readlink "$0")")
+### all valid options
+#OPTIONS=(help start stop restart status reload attach kill)
+### all valid modes
+#MODES=(dev prod test)
+### colors
+#C_RED="\033[31m"
+#C_GREEN="\033[32m"
+#C_YELLOW="\033[33m"
+#C_BLUE="\033[34m"
+#C_DEFAULT="\033[0m"
 
+# show help
+show_help() {
+  echo "Usage: ./sigma.sh [option] <command>"
+  echo "Options:"
+  echo "-m "
+}
 
+# get the process id of this server
+pid() {
+  echo 0
+}
 
-NODE=3
-COOKIE=sigma_server
-NODE_NAME=$COOKIE.$NODE@127.0.0.1
+# show status
+show_status() {
+  echo "status"
+}
 
-#ulimit -SHn 102400
+# stop server
+do_stop() {
+  echo "stop"
+}
 
-# define default configuration
-POLL=true
-SMP=auto
-ERL_MAX_PORTS=32000
-ERL_PROCESSES=500000
-ERL_MAX_ETS_TABLES=1400
+# start server
+do_start() {
+  echo "start"
+}
 
-export ERL_MAX_PORTS
-export ERL_MAX_ETS_TABLES
-export SERVER_BASE
+# restart server
+do_restart() {
+  echo "restart"
+}
 
-DATETIME=$(date "+%Y%m%d%H%M%S")
-LOG_PATH="../logs/app_$NODE.$DATETIME.log"
+# attach to server
+do_attach() {
+  echo "attach"
+}
 
-erl +P $ERL_PROCESSES \
-    +K $POLL \
-    -smp $SMP \
-    -pa ../ebin \
-    -name $NODE_NAME \
-    -setcookie $COOKIE \
-    -boot start_sasl \
-    -config server.config \
-    -kernel error_logger \{file,\""$LOG_PATH"\"\} \
-    -s main server_start
+# reload code
+do_reload() {
+  echo "reload"
+}
+
+# kill the process
+do_kill() {
+  echo "kill"
+}
+
+## main
+if [ $# == 0 ]; then
+    show_help
+    exit 1
+fi
+
+SERVER_MODE=${MODES[1]}
+while getopts ":m::" opt; do
+  case $opt in
+  m)
+    SERVER_MODE="${OPTARG}"
+    ;;
+  \?)
+    show_help
+    ;;
+  esac
+done
+
+#erl +P $ERL_PROCESSES \
+#    +K $POLL \
+#    -smp $SMP \
+#    -pa ../ebin \
+#    -name $NODE_NAME \
+#    -setcookie $COOKIE \
+#    -boot start_sasl \
+#    -config server.config \
+#    -kernel error_logger \{file,\""$LOG_PATH"\"\} \
+#    -s main server_start
