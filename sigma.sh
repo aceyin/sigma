20ac1ba0d919eddb752332a739d902611199bebb
aceyin <aceyin@gmail.com>
2020-03-09 23:00:04 +0800

    add config and start shell

--- sigma.sh
+++ sigma.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-## the full path of this server
-#SERVER_BASE=$(dirname "$(readlink "$0")")
-### all valid options
-#OPTIONS=(help start stop restart status reload attach kill)
-### all valid modes
-#MODES=(dev prod test)
-### colors
-#C_RED="\033[31m"
-#C_GREEN="\033[32m"
-#C_YELLOW="\033[33m"
-#C_BLUE="\033[34m"
-#C_DEFAULT="\033[0m"
+# the full path of this server
+SERVER_BASE=$(dirname "$(readlink "$0")")
+## all valid options
+COMMANDS=(help start stop restart status reload attach kill)
+## all valid modes
+MODES=(dev prod test)
+## colors
+C_RED="\033[31m"
+C_GREEN="\033[32m"
+C_YELLOW="\033[33m"
+C_BLUE="\033[34m"
+C_DEFAULT="\033[0m"
 
 # show help
 show_help() {
@@ -60,13 +60,17 @@ do_kill() {
   echo "kill"
 }
 
-## main
+# server run mode
+SERVER_MODE=${MODES[1]}
+# shell command
+SHELL_COMMAND=${COMMANDS[1]}
+
+## main process
 if [ $# == 0 ]; then
     show_help
     exit 1
 fi
-
-SERVER_MODE=${MODES[1]}
+# read script args
 while getopts ":m::" opt; do
   case $opt in
   m)
@@ -74,9 +78,15 @@ while getopts ":m::" opt; do
     ;;
   \?)
     show_help
+    exit 1
     ;;
   esac
 done
+
+shift $(($OPTIND-1))
+SHELL_COMMAND=$1
+
+# echo "SHELL_COMMAND=$SHELL_COMMAND"
 
 #erl +P $ERL_PROCESSES \
 #    +K $POLL \
